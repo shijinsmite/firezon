f10b6ffd7fcf709e83b69c87cc3f4bb0aff7e340
shijin <vihar57@gmail.com>
2020-08-11 14:52:40 +0530

    products over

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Banner from './images/banner.jpg';
+import Product from './Product';
 import './Home.css';
-
 
 const Home = () => {
   return (
     <div className="home">
-      <img className="home__image" src={Banner} alt="banner"/>
+      <img className="home__image" src={Banner} alt="banner" />
+      <div className="home__row">
+        <Product
+          id="1233"
+          title="javascript wierd parts"
+          price={11.96}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/51x7FR7NP7L._AC_UY218_.jpg"
+        />
+        <Product
+          id="1233"
+          title="javascript wierd parts"
+          price={11.96}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/51x7FR7NP7L._AC_UY218_.jpg"
+        />
+        <Product
+          id="1233"
+          title="javascript wierd parts"
+          price={11.96}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/51x7FR7NP7L._AC_UY218_.jpg"
+        />
+      </div>
     </div>
   );
 };
